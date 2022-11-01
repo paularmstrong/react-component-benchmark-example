ecf6e083b5630115f8bfe24895b8b60de4d16d3a
Paul Armstrong <paul@spaceyak.com>
2022-11-01 08:03:35 -0700

    fix: set build directory

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true
+    appDir: true,
   },
+  distDir: 'dist',
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
